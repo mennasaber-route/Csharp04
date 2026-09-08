8dd8eff03fce186fa82673fe01891ef07ba477c1
Menna Saber <mennasaper@gmail.com>
2026-09-08 08:59:55 +0300

    Add project files.

---
+++ Assignment_3/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace Assignment_3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            #region Question12
+            ////Given int pages = 464; and bool isAvailable = true;, print "You can borrow this book"
+            ////only if pages is greater than 300 and isAvailable is true. Use the && operator
+
+
+            //int pages = 464;
+            //bool isAvailable = true;
+
+            //if(pages > 300 && isAvailable)
+            //{
+            //    Console.WriteLine("You can borrow this book");
+            //}
+
+
+            #endregion
+        }
+    }
+}
