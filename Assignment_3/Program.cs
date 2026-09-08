996a08a50c6d1150ee7c4bf8241ab9f656ed65e0
Menna Saber <mennasaper@gmail.com>
2026-09-08 10:10:31 +0300

    Question20

--- Assignment_3/Program.cs
+++ Assignment_3/Program.cs
@@ -113,6 +113,21 @@
             #endregion
 
 
+            #region Question20
+            ////Using the same books array, print every title except "The Pragmatic Programmer"
+            ////(skip it with continue, don't stop the loop).
+
+
+            //string[] books = { "Clean Code", "The Pragmatic Programmer", "Refactoring" };
+            //foreach (string book in books) { 
+            //    if(book == "The Pragmatic Programmer")
+            //    {
+            //        continue;
+            //    }
+            //    Console.WriteLine(book);
+            //}
+
+            #endregion
         }
 
     }
