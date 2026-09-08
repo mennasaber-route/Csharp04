1c93625b4f31b4a884d9ca775b4106add6eb3c5c
Menna Saber <mennasaper@gmail.com>
2026-09-08 09:57:12 +0300

    Question18

--- Assignment_3/Program.cs
+++ Assignment_3/Program.cs
@@ -78,7 +78,20 @@
             //    i++;
             //}
             //while (i < 3);
-           
+
+
+            #endregion
+
+
+            #region Question18
+            ////Using the same books array, use a foreach loop to print every book title
+
+
+            //string[] books = { "Clean Code", "The Pragmatic Programmer", "Refactoring" };
+            //foreach(string book in books)
+            //{
+            //    Console.WriteLine(book);
+            //}
 
             #endregion
 
